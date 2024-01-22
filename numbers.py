955062a04cd2f1f8842d7793cf281083cd589daa
Iliyas <iliyasmolzhigitov123@gmail.com>
2024-01-22 12:25:42 +0600

    All files were deleted

--- numbers.py
+++
@@ -1,9 +0,0 @@
-"""
-integer1 = 123
-float1 = 123.5
-complex1 = 1j
-"""
-
-import random
-print(random.randrange(1,99))
-
--- variables.py
+++
@@ -1,6 +0,0 @@
-a = "Air Astana"
-b = "FlyArystan"
-x = 1
-y = 2
-print(x + y)
-print(a, b)
