95f61997c300fe85f37bc116b751d430aa94ddac
Iliyas <iliyasmolzhigitov123@gmail.com>
2024-01-22 12:31:12 +0600

    New codes committed

---
+++ 3.py
@@ -0,0 +1,20 @@
+carname = "Volvo"
+
+x = 50
+
+x1  = 5
+y1 = 10
+print(x1+y1)
+
+x2 = 5
+y2 = 10
+z = x2 + y2
+print(z)
+
+x, y, z = "Orange", "Banana", "Cherry"
+
+x = y = z = "Orange"
+
+def myfunc():
+    global x
+    x = "fantastic"
